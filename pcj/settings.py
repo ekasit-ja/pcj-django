fd6173e3c93c4311d997e90aea8b940ba8d9f2e9
Ekasit Ja <ekasit.ja@gmail.com>
2021-03-19 17:15:01 +0700

    initial commit for trying to comply SEO concept

--- pcj/settings.py
+++ pcj/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&s&2uj=_9%q@b&q58x&p64=vg2k61)a-mn*a=x(vke#b)jpeh9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['61.19.247.161', 'localhost', '127.0.0.1', '.pcjindustries.co.th']
 
